998dd66f16d1d645ff674f39b5ccf001c9effbce
Bobholamovic <bob1998425@hotmail.com>
2021-09-06 19:36:40 +0800

    Feat: remove padding layers when pad=0

--- src/models/_blocks.py
+++ src/models/_blocks.py
@@ -11,7 +11,8 @@ class BasicConv(nn.Module):
     def __init__(self, in_ch, out_ch, kernel, pad='zero', bn=False, act=False, **kwargs):
         super().__init__()
         self.seq = nn.Sequential()
-        self.seq.add_module('_pad', getattr(nn, pad.capitalize()+'Pad2d')(kernel//2))
+        if kernel>=2:
+            self.seq.add_module('_pad', getattr(nn, pad.capitalize()+'Pad2d')(kernel//2))
         self.seq.add_module('_conv', nn.Conv2d(
             in_ch, out_ch, kernel,
             stride=1, padding=0,
@@ -81,10 +82,10 @@ class DecBlock(nn.Module):
 
     def forward(self, x1, x2):
         x2 = self.deconv(x2)
-        pl = (x1.size(3)-x2.size(3))//2
-        pr = x1.size(3)-x2.size(3) - pl
-        pt = (x1.size(2)-x2.size(2))//2
-        pb = (x1.size(2)-x2.size(2)) - pt
+        pl = 0
+        pr = x1.size(3)-x2.size(3)
+        pt = 0
+        pb = (x1.size(2)-x2.size(2))
         x2 = F.pad(x2, (pl, pr, pt, pb), 'replicate')
         x = torch.cat((x1, x2), dim=1)
         x = self.conv_feat(x)
