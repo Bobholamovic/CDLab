31c41ec92937d9e34bc108ddc489deffce82f8e2
Bobholamovic <bob1998425@hotmail.com>
2021-10-30 13:50:55 +0800

    Feat: P2V twobranch with lateral connections

--- src/models/p2v.py
+++ src/models/p2v.py
@@ -3,81 +3,116 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models
 
-from ._blocks import DecBlock, ResBlock, Conv3x3, BasicConv
+from ._blocks import (DecBlock, ResBlock, ResBlock2, Conv3x3, MaxPool2x2, BasicConv)
 
 
-class VideoDecoder(nn.Module):
-    def __init__(self, enc_chs, dec_chs):
+class SimpleDecoder(nn.Module):
+    def __init__(self, itm_ch, enc_chs, dec_chs):
         super().__init__()
-        if not len(enc_chs) == len(dec_chs)+1:
-            raise ValueError
+        
         enc_chs = enc_chs[::-1]
-
-        self.convs_video = nn.ModuleList(
-            [
-                BasicConv(2*ch, ch, 1, bn=True, act=True)
-                for ch in enc_chs
-            ]
-        )
         self.blocks = nn.ModuleList([
             DecBlock(in_ch1, in_ch2, out_ch)
-            for in_ch1, in_ch2, out_ch in zip(enc_chs[1:], (enc_chs[0],)+dec_chs[:-1], dec_chs)
+            for in_ch1, in_ch2, out_ch in zip(enc_chs, (itm_ch,)+dec_chs[:-1], dec_chs)
         ])
         self.conv_out = BasicConv(dec_chs[-1], 1, 1, bn=False, act=False)
     
-    def forward(self, *feats):
+    def forward(self, x, feats):
         feats = feats[::-1]
-        x = self.convs_video[0](self.tem_aggr(feats[0]))
-        for blk, conv, f in zip(self.blocks, self.convs_video[1:], feats[1:]):
-            x = blk(conv(self.tem_aggr(f)), x)
+        
+        for feat, blk in zip(feats, self.blocks):
+            x = blk(feat, x)
+
         y = self.conv_out(x)
+
         return y
 
-    def tem_aggr(self, f):
-        return torch.cat([torch.mean(f, dim=2), torch.max(f, dim=2)[0]], dim=1)
 
-
-class VideoSegModel(nn.Module):
-    def __init__(self, in_ch, enc_chs, dec_chs):
+class PairEncoder(nn.Module):
+    def __init__(self, in_ch, enc_chs=(16,32,64), add_chs=(0,0)):
         super().__init__()
-        # Encoder
-        if in_ch != 3 and enc_chs != (64,128,256):
+
+        self.num_layers = 3
+
+        self.conv1 = ResBlock(2*in_ch, enc_chs[0])
+        self.pool1 = MaxPool2x2()
+
+        self.conv2 = ResBlock(enc_chs[0]+add_chs[0], enc_chs[1])
+        self.pool2 = MaxPool2x2()
+
+        self.conv3 = ResBlock2(enc_chs[1]+add_chs[1], enc_chs[2])
+        self.pool3 = MaxPool2x2()
+
+    def forward(self, x1, x2, add_feats=None):
+        x = torch.cat([x1,x2], dim=1)
+        feats = [x]
+
+        for i in range(self.num_layers):
+            conv = getattr(self, f'conv{i+1}')
+            if i > 0 and add_feats is not None:
+                add_feat = F.interpolate(add_feats[i-1], size=x.shape[2:])
+                x = torch.cat([x,add_feat], dim=1)
+            x = conv(x)
+            pool = getattr(self, f'pool{i+1}')
+            x = pool(x)
+            feats.append(x)
+
+        return feats
+
+
+class VideoEncoder(nn.Module):
+    def __init__(self, in_ch, enc_chs=(64,128,256), arch='r3d_18'):
+        super().__init__()
+        if in_ch != 3 or enc_chs != (64,128,256):
             raise NotImplementedError
-        self.encoder = models.video.r3d_18(pretrained=True)
+
+        self.num_layers = 3
+
+        enc_cls = getattr(models.video, arch)
+        self.encoder = enc_cls(pretrained=True)
         self.encoder.layer4 = nn.Identity()
         self.encoder.fc = nn.Identity()
-        # Decoder
-        self.decoder = VideoDecoder(
-            enc_chs=(in_ch, *enc_chs), 
-            dec_chs=dec_chs
-        )
 
     def forward(self, x):
         feats = [x]
+
         x = self.encoder.stem(x)
-        for i in range(3):
+        for i in range(self.num_layers):
             layer = getattr(self.encoder, f'layer{i+1}')
             x = layer(x)
             feats.append(x)
-        out = self.decoder(*feats)
-        return out
+
+        return feats
         
 
 class P2VNet(nn.Module):
-    def __init__(self, in_ch, video_len=8, enc_chs=(64,128,256), dec_chs=(128,64,32)):
+    def __init__(self, in_ch, video_len=8, enc_chs_p=(32,64,128), enc_chs_v=(64,128,256), dec_chs=(256,128,64,32)):
         super().__init__()
         if video_len < 2:
             raise ValueError
         self.video_len = video_len
-        self.seg_video = VideoSegModel(
-            in_ch, 
-            enc_chs=enc_chs,
-            dec_chs=dec_chs
+        self.encoder_p = PairEncoder(in_ch, enc_chs=enc_chs_p, add_chs=enc_chs_v[:-1])
+        self.encoder_v = VideoEncoder(in_ch, enc_chs=enc_chs_v)
+        self.convs_video = nn.ModuleList(
+            [
+                BasicConv(2*ch, ch, 1, bn=True, act=True)
+                for ch in enc_chs_v
+            ]
         )
+        self.decoder = SimpleDecoder(enc_chs_v[-1], (2*in_ch,)+enc_chs_p, dec_chs)
     
     def forward(self, t1, t2):
-        frames = self.pair_to_video(t1, t2)
-        pred = self.seg_video(frames.transpose(1,2))
+        frames = self.pair_to_video(F.interpolate(t1, scale_factor=0.5), F.interpolate(t2, scale_factor=0.5))
+        feats_v = self.encoder_v(frames.transpose(1,2))
+        feats_v.pop(0)
+
+        for i, feat in enumerate(feats_v):
+            feats_v[i] = self.convs_video[i](self.tem_aggr(feat))
+
+        feats_p = self.encoder_p(t1, t2, feats_v[:-1])
+
+        pred = self.decoder(F.interpolate(feats_v[-1], feats_p[-1].shape[2:]), feats_p)
+
         return pred
 
     def pair_to_video(self, im1, im2, rate_map=None):
@@ -92,3 +127,6 @@ class P2VNet(nn.Module):
             rate_map = torch.ones_like(im1[:,0:1])
         frames = _interpolate(im1, im2, rate_map, self.video_len)
         return frames
+
+    def tem_aggr(self, f):
+        return torch.cat([torch.mean(f, dim=2), torch.max(f, dim=2)[0]], dim=1)
