8fee22704ac670c0ba0911b9f12abd67a8ee2533
Bobholamovic <bob1998425@hotmail.com>
2021-12-24 14:31:37 +0800

    Feat: FC nets use dropout by default

--- src/models/unet.py
+++ src/models/unet.py
@@ -14,65 +14,84 @@ from torch.nn.modules.padding import ReplicationPad2d
 class Unet(nn.Module):
     """EF segmentation network."""
 
-    def __init__(self, input_nbr, label_nbr):
+    def __init__(self, input_nbr, label_nbr, use_dropout=True):
         super(Unet, self).__init__()
 
         self.input_nbr = input_nbr
+        self.use_dropout = use_dropout
 
         self.conv11 = nn.Conv2d(input_nbr, 16, kernel_size=3, padding=1)
         self.bn11 = nn.BatchNorm2d(16)
+        self.do11 = self.make_dropout()
         self.conv12 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
         self.bn12 = nn.BatchNorm2d(16)
+        self.do12 = self.make_dropout()
 
         self.conv21 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.bn21 = nn.BatchNorm2d(32)
+        self.do21 = self.make_dropout()
         self.conv22 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
         self.bn22 = nn.BatchNorm2d(32)
+        self.do22 = self.make_dropout()
 
         self.conv31 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.bn31 = nn.BatchNorm2d(64)
+        self.do31 = self.make_dropout()
         self.conv32 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
         self.bn32 = nn.BatchNorm2d(64)
+        self.do32 = self.make_dropout()
         self.conv33 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
         self.bn33 = nn.BatchNorm2d(64)
+        self.do33 = self.make_dropout()
 
         self.conv41 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.bn41 = nn.BatchNorm2d(128)
+        self.do41 = self.make_dropout()
         self.conv42 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
         self.bn42 = nn.BatchNorm2d(128)
+        self.do42 = self.make_dropout()
         self.conv43 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
         self.bn43 = nn.BatchNorm2d(128)
-
+        self.do43 = self.make_dropout()
 
         self.upconv4 = nn.ConvTranspose2d(128, 128, kernel_size=3, padding=1, stride=2, output_padding=1)
 
         self.conv43d = nn.ConvTranspose2d(256, 128, kernel_size=3, padding=1)
         self.bn43d = nn.BatchNorm2d(128)
+        self.do43d = self.make_dropout()
         self.conv42d = nn.ConvTranspose2d(128, 128, kernel_size=3, padding=1)
         self.bn42d = nn.BatchNorm2d(128)
+        self.do42d = self.make_dropout()
         self.conv41d = nn.ConvTranspose2d(128, 64, kernel_size=3, padding=1)
         self.bn41d = nn.BatchNorm2d(64)
+        self.do41d = self.make_dropout()
 
         self.upconv3 = nn.ConvTranspose2d(64, 64, kernel_size=3, padding=1, stride=2, output_padding=1)
 
         self.conv33d = nn.ConvTranspose2d(128, 64, kernel_size=3, padding=1)
         self.bn33d = nn.BatchNorm2d(64)
+        self.do33d = self.make_dropout()
         self.conv32d = nn.ConvTranspose2d(64, 64, kernel_size=3, padding=1)
         self.bn32d = nn.BatchNorm2d(64)
+        self.do32d = self.make_dropout()
         self.conv31d = nn.ConvTranspose2d(64, 32, kernel_size=3, padding=1)
         self.bn31d = nn.BatchNorm2d(32)
+        self.do31d = self.make_dropout()
 
         self.upconv2 = nn.ConvTranspose2d(32, 32, kernel_size=3, padding=1, stride=2, output_padding=1)
 
         self.conv22d = nn.ConvTranspose2d(64, 32, kernel_size=3, padding=1)
         self.bn22d = nn.BatchNorm2d(32)
+        self.do22d = self.make_dropout()
         self.conv21d = nn.ConvTranspose2d(32, 16, kernel_size=3, padding=1)
         self.bn21d = nn.BatchNorm2d(16)
+        self.do21d = self.make_dropout()
 
         self.upconv1 = nn.ConvTranspose2d(16, 16, kernel_size=3, padding=1, stride=2, output_padding=1)
 
         self.conv12d = nn.ConvTranspose2d(32, 16, kernel_size=3, padding=1)
         self.bn12d = nn.BatchNorm2d(16)
+        self.do12d = self.make_dropout()
         self.conv11d = nn.ConvTranspose2d(16, label_nbr, kernel_size=3, padding=1)
 
     def forward(self, x1, x2):
@@ -81,56 +100,61 @@ class Unet(nn.Module):
 
         """Forward method."""
         # Stage 1
-        x11 = F.relu(self.bn11(self.conv11(x)))
-        x12 = F.relu(self.bn12(self.conv12(x11)))
+        x11 = self.do11(F.relu(self.bn11(self.conv11(x))))
+        x12 = self.do12(F.relu(self.bn12(self.conv12(x11))))
         x1p = F.max_pool2d(x12, kernel_size=2, stride=2)
 
         # Stage 2
-        x21 = F.relu(self.bn21(self.conv21(x1p)))
-        x22 = F.relu(self.bn22(self.conv22(x21)))
+        x21 = self.do21(F.relu(self.bn21(self.conv21(x1p))))
+        x22 = self.do22(F.relu(self.bn22(self.conv22(x21))))
         x2p = F.max_pool2d(x22, kernel_size=2, stride=2)
 
         # Stage 3
-        x31 = F.relu(self.bn31(self.conv31(x2p)))
-        x32 = F.relu(self.bn32(self.conv32(x31)))
-        x33 = F.relu(self.bn33(self.conv33(x32)))
+        x31 = self.do31(F.relu(self.bn31(self.conv31(x2p))))
+        x32 = self.do32(F.relu(self.bn32(self.conv32(x31))))
+        x33 = self.do33(F.relu(self.bn33(self.conv33(x32))))
         x3p = F.max_pool2d(x33, kernel_size=2, stride=2)
 
         # Stage 4
-        x41 = F.relu(self.bn41(self.conv41(x3p)))
-        x42 = F.relu(self.bn42(self.conv42(x41)))
-        x43 = F.relu(self.bn43(self.conv43(x42)))
+        x41 = self.do41(F.relu(self.bn41(self.conv41(x3p))))
+        x42 = self.do42(F.relu(self.bn42(self.conv42(x41))))
+        x43 = self.do43(F.relu(self.bn43(self.conv43(x42))))
         x4p = F.max_pool2d(x43, kernel_size=2, stride=2)
-
 
         # Stage 4d
         x4d = self.upconv4(x4p)
         pad4 = ReplicationPad2d((0, x43.size(3) - x4d.size(3), 0, x43.size(2) - x4d.size(2)))
         x4d = torch.cat((pad4(x4d), x43), 1)
-        x43d = F.relu(self.bn43d(self.conv43d(x4d)))
-        x42d = F.relu(self.bn42d(self.conv42d(x43d)))
-        x41d = F.relu(self.bn41d(self.conv41d(x42d)))
+        x43d = self.do43d(F.relu(self.bn43d(self.conv43d(x4d))))
+        x42d = self.do42d(F.relu(self.bn42d(self.conv42d(x43d))))
+        x41d = self.do41d(F.relu(self.bn41d(self.conv41d(x42d))))
 
         # Stage 3d
         x3d = self.upconv3(x41d)
         pad3 = ReplicationPad2d((0, x33.size(3) - x3d.size(3), 0, x33.size(2) - x3d.size(2)))
         x3d = torch.cat((pad3(x3d), x33), 1)
-        x33d = F.relu(self.bn33d(self.conv33d(x3d)))
-        x32d = F.relu(self.bn32d(self.conv32d(x33d)))
-        x31d = F.relu(self.bn31d(self.conv31d(x32d)))
+        x33d = self.do33d(F.relu(self.bn33d(self.conv33d(x3d))))
+        x32d = self.do32d(F.relu(self.bn32d(self.conv32d(x33d))))
+        x31d = self.do31d(F.relu(self.bn31d(self.conv31d(x32d))))
 
         # Stage 2d
         x2d = self.upconv2(x31d)
         pad2 = ReplicationPad2d((0, x22.size(3) - x2d.size(3), 0, x22.size(2) - x2d.size(2)))
         x2d = torch.cat((pad2(x2d), x22), 1)
-        x22d = F.relu(self.bn22d(self.conv22d(x2d)))
-        x21d = F.relu(self.bn21d(self.conv21d(x22d)))
+        x22d = self.do22d(F.relu(self.bn22d(self.conv22d(x2d))))
+        x21d = self.do21d(F.relu(self.bn21d(self.conv21d(x22d))))
 
         # Stage 1d
         x1d = self.upconv1(x21d)
         pad1 = ReplicationPad2d((0, x12.size(3) - x1d.size(3), 0, x12.size(2) - x1d.size(2)))
         x1d = torch.cat((pad1(x1d), x12), 1)
-        x12d = F.relu(self.bn12d(self.conv12d(x1d)))
+        x12d = self.do12d(F.relu(self.bn12d(self.conv12d(x1d))))
         x11d = self.conv11d(x12d)
 
         return x11d
+
+    def make_dropout(self):
+        if self.use_dropout:
+            return nn.Dropout2d(p=0.2)
+        else:
+            return nn.Identity()
