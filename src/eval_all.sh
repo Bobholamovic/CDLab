e368d5ef13538df772226abac8140228b0744b5e
Bobholamovic <bob1998425@hotmail.com>
2022-01-13 14:11:05 +0800

    feat: eval_all add sw_test

--- src/eval_all.sh
+++ src/eval_all.sh
@@ -2,7 +2,7 @@
 
 export CUDA_VISIBLE_DEVICES=2
 
-MODELS=(cdnet unet siamunet-conc siamunet-diff stanet lunet bit ifn snunet p2v)
+MODELS=(cdnet unet siamunet-conc siamunet-diff stanet lunet bit ifn snunet)
 DATASETS=(svcd levircd whu)
 
 
@@ -11,11 +11,38 @@ for dataset in "${DATASETS[@]}"
 do
     for model in "${MODELS[@]}"
     do
-        if [ dataset = 'svcd' ] && [ model = 'p2v' ]
+        if [ ${dataset} = 'svcd' ] && [ ${model} = 'p2v' ]
         then
             cfg_path='p2v/config_svcd_p2v.yaml' 
         else
             cfg_path="config_${dataset}_${model}.yaml"
+        fi
+        if [ ${dataset} = 'levircd' ]
+        then
+            if [ ${model} = 'p2v' ]
+            then
+                python sw_test.py --exp_config "../configs/${dataset}/${cfg_path}" \
+                    --ckp_path "../exp/${dataset}/weights/model_best_${model}.pth" \
+                    --t1_dir "/home/gdf/data/LEVIR-CD/ori/test/A" \
+                    --t2_dir "/home/gdf/data/LEVIR-CD/ori/test/B" \
+                    --gt_dir "/home/gdf/data/LEVIR-CD/ori/test/label" \
+                    --out_dir "../exp/levircd/sw_out/${model}" \
+                    --save_on \
+                    --stride 64 \
+                    --mu 110.2008 100.63983 95.99475 \
+                    --sigma 58.14765 56.46975 55.332195
+            else
+                python sw_test.py --exp_config "../configs/${dataset}/${cfg_path}" \
+                    --ckp_path "../exp/${dataset}/weights/model_best_${model}.pth" \
+                    --t1_dir "/home/gdf/data/LEVIR-CD/ori/test/A" \
+                    --t2_dir "/home/gdf/data/LEVIR-CD/ori/test/B" \
+                    --gt_dir "/home/gdf/data/LEVIR-CD/ori/test/label" \
+                    --out_dir "../exp/levircd/sw_out/${model}" \
+                    --save_on \
+                    --stride 64 \
+                    --mu 0.0 0.0 0.0 \
+                    --sigma 255.0 255.0 255.0
+            fi
         fi
         python train.py eval --exp_config "../configs/${dataset}/${cfg_path}" \
             --resume "../exp/${dataset}/weights/model_best_${model}.pth" \
