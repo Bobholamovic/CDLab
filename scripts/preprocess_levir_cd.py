e4b5cb3736e4abb4fd6c7646e1f3dcabc22d5d1d
Bobholamovic <bob1998425@hotmail.com>
2022-01-16 13:25:04 +0800

    feat: rearrange dataset scripts

--- scripts/preprocess_levir_cd.py
+++ scripts/preprocess_levir_cd.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 
 
 CROP_SIZE = 256
-STRIDE = 128
+STRIDE = 256
 
 
 if __name__ == '__main__':
@@ -28,7 +28,7 @@ if __name__ == '__main__':
                 out_subdir = join(out_dir, subset, subdir, name)
                 if not exists(out_subdir):
                     makedirs(out_subdir)
-                stride = STRIDE if subset == 'train' else STRIDE*2
+                stride = STRIDE//2 if subset == 'train' else STRIDE
                 for i in range(0, h-CROP_SIZE+1, stride):
                     for j in range(0, w-CROP_SIZE+1, stride):
                         imsave(join(out_subdir, '{}_{}{}'.format(name,next(counter),ext)), img[i:i+CROP_SIZE, j:j+CROP_SIZE])
