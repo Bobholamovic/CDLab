90e6a78c119b8882353043627edfe0d912c08ac4
Bobholamovic <bob1998425@hotmail.com>
2022-02-02 17:53:19 +0800

    feat: use fan_in for kaiming init

--- src/models/_utils.py
+++ src/models/_utils.py
@@ -7,7 +7,8 @@ class KaimingInitMixin:
     def _init_weight(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                # By default use fan_in mode and leaky relu non-linearity with a=0
+                nn.init.kaiming_normal_(m.weight)
             elif isinstance(m, (nn.BatchNorm2d, nn.SyncBatchNorm)):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
